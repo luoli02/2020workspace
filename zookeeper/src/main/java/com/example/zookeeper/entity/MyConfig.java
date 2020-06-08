9f2dc13ed247797dc85eec7e134fc1056856544b
luoli <1377416424@qq.com>
2020-06-09 01:21:29 +0800

    idea提交

--- zookeeper/src/main/java/com/example/zookeeper/entity/MyConfig.java
+++ zookeeper/src/main/java/com/example/zookeeper/entity/MyConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(value = {MyImportRegistrar.class})
+@Import(value = {Person.class, MySelector.class, MyImportRegistrar.class})
 public class MyConfig {
 
 }
